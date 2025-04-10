0de73cc40d5251200141cae41c4678d3777d0acb
Vb756680 <abdurrahman@smkwikrama.sch.id>
2025-04-10 14:05:06 +0700

    add middleware | 4/10/2025

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,35 @@
-import { createBrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 import Template from "../layouts/template";
 import Dashboard from "../pages/Dashboard";
 import PrivatePage from "../pages/middleware/PrivatePage";
+import GuestPage from "../pages/middleware/GuestPage";
 
 export const router = createBrowserRouter([
-  // path : url yang dipakai, element: views yang akan ditampilkan
-
   {
     path: "/",
     element: <Template />,
     children: [
-      { path: "/", element: <App /> },
-      { path: "/login", element: <Login /> },
-      { path: "/profile", element: <Profile /> },
-      {
-        path: "/dashboard", element: <PrivatePage/>,
-        // route pada children, route yang dibatasi akses nya
-        children : [
-          {path: "/", element: <Dashboard />},
-        ]
-
+      { 
+        path: "/", 
+        element: <App /> 
       },
+      { 
+        path: "/login", 
+        element: <GuestPage />,
+        children : [
+          {path: "/login", element: <Login />},
+        ] 
+      },
+      {
+        element: <PrivatePage />,
+        children: [
+          { path: "profile", element: <Profile /> },
+          { path: "dashboard", element: <Dashboard /> }
+        ]
+      }
     ],
-},
+  },
 ]);
